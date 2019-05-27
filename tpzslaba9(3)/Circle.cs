ca99ba3c6837981e4ef4eda5fdad7cff7b2b9257
tenickajaalena <tenickajaalena@gmail.com>
2019-05-27 23:39:07 +0300

    Добавьте файлы проекта.

---
+++ tpzslaba9(3)/Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace tpzslaba9_3_
+{
+    class Circle
+    {
+        public double SquareCircle(double radius)
+        {
+
+            return Math.PI * Math.Pow(radius, 2);
+
+        }
+
+        public double LengthCircle(double radius)
+        {
+
+            return 2 * Math.PI * radius;
+
+        }
+
+        public double CapacitySphere(double radius)
+        {
+
+            return ((double)4 / 3) * Math.PI * Math.Pow(radius, 3);
+
+        }
+    }
+}
---
+++ tpzslaba9(3)/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace tpzslaba9_3_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<double, double> f;
+
+            Console.WriteLine("Введите радиус для фигур : ");
+
+            double radius = Convert.ToDouble(Console.ReadLine());
+
+            Circle circle = new Circle();
+
+            f = circle.SquareCircle;
+            Console.WriteLine("Площадь круга - {0}", f.Invoke(radius));
+
+            f = circle.LengthCircle;
+            Console.WriteLine("Длина круга - {0}", f.Invoke(radius));
+
+            f = circle.CapacitySphere;
+            Console.WriteLine("Обьем сферы - {0}", f.Invoke(radius));
+            Console.ReadKey();
+        }
+    }
+}
